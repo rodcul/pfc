0e025668343acf6733d5b95ceec5fc0fcb878241
Rodney Cullen <rodcul@gmail.com>
2014-05-22 20:21:22 +0200

    Script para actualizar urls al pasar de entorno desarrollo a producción

---
+++ db/migration_script.sql
@@ -0,0 +1,7 @@
+UPDATE wp_options SET option_value = replace(option_value, 'http://dev.pfc.rodcul.com', 'http://pfc.rodcul.com') WHERE option_name = 'home' OR option_name = 'siteurl';
+
+UPDATE wp_posts SET guid = replace(guid, 'http://dev.pfc.rodcul.com','http://pfc.rodcul.com');
+
+UPDATE wp_posts SET post_content = replace(post_content, 'http://dev.pfc.rodcul.com', 'http://pfc.rodcul.com');
+
+UPDATE wp_postmeta SET meta_value = replace(meta_value,'http://dev.pfc.rodcul.com','http://pfc.rodcul.com');
