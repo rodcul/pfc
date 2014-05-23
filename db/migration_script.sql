5ac00ede9ed72324bafb08ef21800a618c9a58c4
Rodney Cullen <rodcul@gmail.com>
2014-05-23 23:28:57 +0200

    Script migración actualiza url de videotube

--- db/migration_script.sql
+++ db/migration_script.sql
@@ -1,7 +1,8 @@
-UPDATE wp_options SET option_value = replace(option_value, 'http://dev.pfc.rodcul.com', 'http://pfc.rodcul.com') WHERE option_name = 'home' OR option_name = 'siteurl';
+UPDATE wp_options SET option_value = replace(option_value, 'http://dev.pfc.rodcul.com', 'http://pfc.rodcul.com') WHERE option_name = 'home' OR option_name = 'siteurl' OR option_name = 'videotube';
 
 UPDATE wp_posts SET guid = replace(guid, 'http://dev.pfc.rodcul.com','http://pfc.rodcul.com');
 
 UPDATE wp_posts SET post_content = replace(post_content, 'http://dev.pfc.rodcul.com', 'http://pfc.rodcul.com');
 
 UPDATE wp_postmeta SET meta_value = replace(meta_value,'http://dev.pfc.rodcul.com','http://pfc.rodcul.com');
+
